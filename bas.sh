332b934a0a2d8c26eaa8281a864eba917ac19c13
kanthchandraship <kanthchandraship@gmail.com>
2017-02-14 22:25:14 +0530

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,11 +1,25 @@
-git --version
-version=`git --version | awk '{print $3}'`
-echo $version
-version=( ${version//./ } )
-echo ${version[0]}
-echo ${version[1]}
-if [ "${version[0]}" -gt "1" ] || ( [ "${version[0]}" -eq "1" ] && [ "${version[1]}" -gt "8" ] ); then
-  echo "Compatible"
-else 
-  echo "Not compatible"
-fi;
+#!/bin/bash -e
+
+shippable_decrypt() {
+  local SECURE_FILE=$1
+  local KEY_FILE=$2
+  local TEMP_DEST='/home/shippable/decrypt'
+
+  if [ -d "$TEMP_DEST" ]; then
+    rm -r ${TEMP_DEST:?}
+  fi
+  mkdir -p $TEMP_DEST/fragments
+
+  base64 --decode < "$SECURE_FILE" > $TEMP_DEST/encrypted.raw
+  split -b 256 "$TEMP_DEST/encrypted.raw" $TEMP_DEST/fragments/
+  local fragments
+  fragments=$(ls -b $TEMP_DEST/fragments)
+  for fragment in $fragments; do
+    openssl rsautl -decrypt -inkey "$KEY_FILE" -oaep < "$TEMP_DEST/fragments/$fragment" >> "$SECURE_FILE.decrypted"
+  done;
+
+  rm -r ${TEMP_DEST:?}/*
+}
+
+cat /tmp/ssh/00_sub
+#shippable_decrypt "$1" "$2"
